82de1d68e332c337fcb1d08341f2e4dbb4fec4f5
maitreyi97 <maitreyi@ciencias.unam.mx>
2025-09-18 17:36:11 -0600

    práctica 4 implementada

--- product/src/main/java/com/product/api/service/SvcCategory.java
+++ product/src/main/java/com/product/api/service/SvcCategory.java
@@ -1,11 +1,12 @@
 package com.product.api.service;
 
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import com.product.api.entity.Category;
 
 /**
  * Interfaz SvcCategory que define el servicio para las categorías
  */
 public interface SvcCategory {
-    public List<Category> getCategories();
+    public ResponseEntity<List<Category>> getCategories();
 }
